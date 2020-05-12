2df6605d15af3a036ebfa23d200f6ed06bb3c009
sirokumasan <fsho2262@gmail.com>
2020-05-12 16:00:32 +0900

    model_upload

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,4 @@
 class Like < ApplicationRecord
-  belongs_to :user
-  belongs_to :message
+  belongs_to :user  
+  belongs_to :message, counter_caches :like_count
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -6,4 +6,7 @@ class Message < ApplicationRecord
   has_many   :liking_users,  through: :liks, source: :user
   accepts_nested_attributes_for :images, allow_destroy: true
   validates :content, presence: true
+  def like_user(user_id)
+    Likes.find_by(user_id: user_id)
+  end
 end
