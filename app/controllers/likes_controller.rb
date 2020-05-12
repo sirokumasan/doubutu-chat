e5a8830e7090fbf52b52614c265a31349178701c
sirokumasan <fsho2262@gmail.com>
2020-05-12 14:47:22 +0900

    Update likes_controller.rb

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,19 @@
 class LikesController < ApplicationController
+  before_action :set_variables
+
+  def like
+    @like = current_user.likes.new(message_id: @message.id)
+    @lile.save
+  end
+
+  def unlike
+    like = current_user.likes.find_by(message_id: @message.id)
+    like.destroy
+  end
+
+  privat
+  def set_variables
+    @message = Message.find(params[:message_id])
+  end
+
 end
