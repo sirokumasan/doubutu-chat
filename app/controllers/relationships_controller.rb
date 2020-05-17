55d5bbb691a8cb2b04a39a8153d494a18f08b6c7
sirokumasan <fsho2262@gmail.com>
2020-05-17 09:43:36 +0900

    javascript.js

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,21 +3,24 @@ class RelationshipsController < ApplicationController
 
   #POsT/relationships
   def create
-    #binding.pry
     @user = User.find(params[:relationship][:followed_id])
     current_user.follow(@user)
     respond_to do |format| 
       format.html {redirect_to @user}
       format.js
+    end
+    #binding.pry
   end
 
   # delete/relationships/:id
   def destroy  
     @user = Relationship.find(params[:id]).followed
-    current_user.unfollow(user)
+    current_user.unfollow(@user)
     respond_to do |format|
       format.html {redirect_to @user}
       format.js
+    end
+    #binding.pry
   end
 
 end
