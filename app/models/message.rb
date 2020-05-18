a16402d08b16ef8eb48b3c854d49180f41627c4b
sirokumasan <fsho2262@gmail.com>
2020-05-18 13:30:26 +0900

    Update message.rb

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,13 +1,13 @@
 class Message < ApplicationRecord
   belongs_to :user
-  has_many   :images,       dependent: :destroy
+  has_many   :images,           dependent: :destroy
   has_many   :comments
-  has_many   :likes,        dependent: :destroy
-  has_many   :liked_users,  through: :likes, source: :user
-  has_many   :tags,         dependent: :destroy
+  has_many   :likes,            dependent: :destroy
+  has_many   :liked_users,      through: :likes, source: :user
+  has_many   :message_tags,     dependent: :destroy
+  has_many   :tags,             through: :message_tag
   
   accepts_nested_attributes_for :images, allow_destroy: true
-  accepts_nested_attributes_for :tags,   allow_destroy: true
   validates :content, presence: true
   is_impressionable counter_cache: true
 
