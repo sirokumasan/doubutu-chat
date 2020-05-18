a8a8c5b234347fbed8b9172e0897819e0a502c6c
sirokumasan <fsho2262@gmail.com>
2020-05-18 11:53:08 +0900

    Update message.rb

--- app/models/message.rb
+++ app/models/message.rb
@@ -7,6 +7,7 @@ class Message < ApplicationRecord
   has_many   :tags,         dependent: :destroy
   
   accepts_nested_attributes_for :images, allow_destroy: true
+  accepts_nested_attributes_for :tags,   allow_destroy: true
   validates :content, presence: true
   is_impressionable counter_cache: true
 
