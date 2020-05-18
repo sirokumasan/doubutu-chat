7c0e6066720df9e87421f336dd01b97c4b9ebaf4
sirokumasan <fsho2262@gmail.com>
2020-05-18 22:35:12 +0900

    Update messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,6 +3,7 @@ class MessagesController < ApplicationController
     @messages = Message.includes(:user)
     @all_ranks = Message.find(Like.group(:message_id).order('count(message_id) desc').limit(10).count(:message_id).keys)
     @most_views = Message.order("impressions_count DESC").take(10)
+    @list_tags = Tag.include(:message)
   end
 
   def new
