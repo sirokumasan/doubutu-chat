54c4c99da39e32f336b8669f1b69c7cee14a5ac0
sirokumasan <fsho2262@gmail.com>
2020-06-13 10:25:34 +0900

    tag_controller index追加

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,8 @@
 class TagsController < ApplicationController
+
+  def index
+    @tags = Tag.where(tag_name: params[:tag_name])
+    # @list_tags = Tag.joins(:message_tags).group(:tag_id).order('count(tag_id) DESC')
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   end
   post    '/like/:message_id' => 'likes#like', as: 'like'
   delete  '/like/:message_id' => 'likes#unlike', as: 'unlike'
-  resources :tags, only: :index
+  get     '/tag/:tag_name' => 'tags#index', as: 'tags'
   get    '/login'   => 'sessions#new'
   post   '/login'   => 'sessions#create'
   delete '/logout'  => 'sessions#destroy'
